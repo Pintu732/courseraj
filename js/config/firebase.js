399347c852a7e645f0cf3c0eb61b0fac8083bd0b
Pintu732 <119785346+Pintu732@users.noreply.github.com>
2026-08-09 15:18:43 +0530

    Upload Firebase config

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -1,1 +1,19 @@
-// Firebase config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjQlaJqnl0IYSph7qVFSrN5icZtwzcwDY",
+  authDomain: "skillvault-rajeeb.firebaseapp.com",
+  projectId: "skillvault-rajeeb",
+  storageBucket: "skillvault-rajeeb.firebasestorage.app",
+  messagingSenderId: "736105036244",
+  appId: "1:736105036244:web:28250315ce5cdf64d67e41",
+  measurementId: "G-ZM85BXFSBC"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+export const ADMIN_EMAIL = "vovanew76@gmail.com";
